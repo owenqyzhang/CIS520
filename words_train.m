01bd34da1a62452e2b4f72b77dd8ee6ab9cdd2e8
Owen Zhang <owenqyzhang@gmail.com>
2016-11-27 18:03:09 -0500

    Predict labels using logistic regression

--- words_train.m
+++ words_train.m
@@ -34,28 +34,36 @@ X_pca = X * coeff;
 % end
 
 %% Semisupervised
+addpath('liblinear/');
+log_ori = logistic(X1, full(Y), [], []);
+log_pca = logistic(X_pca(1: 4500, :), full(Y), [], []);
+Yhat = predict_labels(X2, [], [], [], [], []);
+y_unlabeled_est = predict(ones(4500, 1), sparse(X2), log_ori, ['-q', 'col']);
+Y = [full(Y); y_unlabeled_est];
+log_ori_full = logistic(X, Y, [], []);
+log_pca_full = logistic(X_pca, Y, [], []);
+
 % load ./models/logistic_1.mat
-% y_unlabeled_est = 
-precision_ori_log = zeros(9, 1);
-precision_pca_log = zeros(9, 1);
-for i = 1: 9
-    idx = 1: 4500;
-    idx_test = randperm(4500, 500);
-    idx_train = idx;
-    idx_train(idx_test) = [];
-    Y = full(Y) + 1;
-    [B_ori, dev_ori, stats_ori] = mnrfit(X(idx_train, :),...
-        Y(idx_train));
-    ori_pred = mnrval(B_ori, X(idx_test, :));
-    Yhat_ori = ori_pred(:, 2) >= 0.5;
-    precision_ori_log(i) = mean(Yhat_ori == Y(idx_test));
-    [B_pca, dev_pca, stats_pca] = mnrfit(X_pca(idx_train, :),...
-        Y(idx_train));
-    pca_pred = mnrval(B_pca, X_pca(idx_test, :));
-    Yhat_pca = pca_pred(:, 2) >= 0.5;
-    precision_pca_log(i) = mean(Yhat_pca == Y(idx_test));
-end
-precision_ori_log_ave = mean(precision_ori_log);
-precision_pca_log_ave = mean(precision_pca_log);
-% log_ori = logistic(full(X), full(Y), [], []);
-% log_pca = logistic(X_pca, full(Y), [], []);
+
+% precision_ori_log = zeros(9, 1);
+% precision_pca_log = zeros(9, 1);
+% for i = 1: 9
+%     idx = 1: 4500;
+%     idx_test = randperm(4500, 500);
+%     idx_train = idx;
+%     idx_train(idx_test) = [];
+%     Y = full(Y) + 1;
+%     [B_ori, dev_ori, stats_ori] = mnrfit(X(idx_train, :),...
+%         Y(idx_train));
+%     ori_pred = mnrval(B_ori, X(idx_test, :));
+%     Yhat_ori = ori_pred(:, 2) >= 0.5;
+%     precision_ori_log(i) = mean(Yhat_ori == Y(idx_test));
+%     [B_pca, dev_pca, stats_pca] = mnrfit(X_pca(idx_train, :),...
+%         Y(idx_train));
+%     pca_pred = mnrval(B_pca, X_pca(idx_test, :));
+%     Yhat_pca = pca_pred(:, 2) >= 0.5;
+%     precision_pca_log(i) = mean(Yhat_pca == Y(idx_test));
+% end
+% precision_ori_log_ave = mean(precision_ori_log);
+% precision_pca_log_ave = mean(precision_pca_log);
+
