5a0f75767d8ac799fa9014dfcfbc36032025602e
Owen Zhang <owenqyzhang@gmail.com>
2016-11-27 22:15:01 -0500

    Naive Bayes semi-supervised method

--- words_train.m
+++ words_train.m
@@ -7,78 +7,34 @@ load ./data/train_set_unlabeled/words_train_unlabeled.mat
 X2 = full(X);
 X = [X1; X2];
 load ./models/coeff.mat
-X_pca = X * coeff(:, 1: 750);
-% load ./train_set/words_train_pca.mat
-% load ./train_set/words_train_rbm.mat
 
-%%
-% idx = 1: 9000;
-% idx_test = randperm(9000, 900);
-% idx_train = idx;
-% idx_train(idx_test) = [];
-% [coeff, score_train, score_test, numpc] = ...
-%     pca_getpc(X(idx_train, :), X(idx_test, :));
-
-
-%% Bayes GMM
-% mu_test = mean(test_pca);
-% 
-% uni = [1, 1];
-% alt = [sum(full(Y(1: 4000)) == 0), sum(full(Y(1: 4000)) == 0)];
-% 
-% clusters = [50, 100, 200];
-% bayes_gmm_acc = zeros(2, 3);
-% for i = 1: 3
-%     bayes_gmm_acc(1, i) = bayes_gmm_err(train_pca, trainY, test_pca, testY, uni, clusters(i));
-%     bayes_gmm_acc(2, i) = bayes_gmm_err(train_pca, trainY, test_pca, testY, alt, clusters(i));
-% end
-
-%% Semisupervised
-addpath('liblinear/');
-log_ori = train(full(Y), sparse(X1), ['-s 0', 'col']);
-% log_ori = logistic(X1, full(Y), [], []);
-% Yhat = predict_labels(X2, [], [], [], [], []);
-y_unlabeled_est = predict(ones(4500, 1), sparse(X2), log_ori, ['-q', 'col']);
-Y = [full(Y); y_unlabeled_est];
-log_ori_full = train(full(Y), sparse(X), ['-s 0', 'col']);
-save('./models/log_ori.mat', 'log_ori', '-v7.3');
-
-% precision_ori_log = zeros(9, 1);
-% precision_pca_log = zeros(9, 1);
-% ind = crossvalind('Kfold', 4500, 10);
-% for i = 1: 10
-%     idx = 1: 9000;
-%     idx_test = find(ind == i);
-%     idx_train = idx;
-%     idx_train(idx_test) = [];
-% 
-%     [~, precision_ori_log(i)] = logistic(X(idx_train, :), Y(idx_train),...
-%         X(idx_test, :), Y(idx_test));
-%     [~, precision_pca_log(i)] = logistic(X_pca(idx_train, :), Y(idx_train),...
-%         X_pca(idx_test, :), Y(idx_test));
-% end
-% precision_ori_log_ave = mean(precision_ori_log);
-% precision_pca_log_ave = mean(precision_pca_log);
+%% PCA Dimensioin Reduction
+% cov_train = cov(X);
+% [coeff_train, latent] = pcacov(cov_train);
+score_train = X * coeff_train(:, 1: 750);
 
 %% Naive Bayes
-% Mdl_pca = fitcnb(X_pca(1: 4500, 1: 750), Y);
+Mdl_pca = fitcnb(score_train(1: 4500, :), Y);
 % save('./models/nb_pca.mat', 'Mdl_pca', '-v7.3');
-% precision_ori_nb = zeros(9, 1);
-% precision_pca_nb = zeros(9, 1);
-% ind = crossvalind('Kfold', 4500, 10);
-% for i = 1: 10
-%     X_ori_train = X_pca(ind ~= i, :);
-%     X_pca_train = X_pca(ind ~= i, 1: 750);
-%     Y_train = Y(ind ~= i);
-%     X_ori_test = X_pca(ind == i, :);
-%     X_pca_test = X_pca(ind == i, 1: 750);
-%     Y_test = Y(ind == i); 
-%     Mdl_ori = fitcnb(X_ori_train, Y_train);
-%     Mdl_pca = fitcnb(X_pca_train, Y_train);
-%     Yhat_ori = predict(Mdl_ori, X_ori_test);
-%     Yhat_pca = predict(Mdl_pca, X_pca_test);
-%     precision_ori_nb(i) = mean(Yhat_ori == Y_test);
-%     precision_pca_nb(i) = mean(Yhat_pca == Y_test);
-% end
-% precision_ori_nb_ave = mean(precision_ori_nb);
-% precision_pca_nb_ave = mean(precision_pca_nb);
+y_unlabeled_est = predict(Mdl_pca, score_train(4501: 9000, :));
+Y = [full(Y); y_unlabeled_est];
+Mdl_pca_full = fitcnb(score_train(:, :), Y);
+save('./models/nb_pca_full.mat', 'Mdl_pca', '-v7.3');
+
+precision_pca_nb = zeros(9, 1);
+ind = crossvalind('Kfold', 4500, 10);
+for i = 1: 10
+    idx = 1: 9000;
+    idx_test = find(ind == i);
+    idx_train = idx;
+    idx_train(idx_test) = [];
+    
+    X_pca_train = score_train(idx_train, :);
+    Y_train = Y(idx_train);
+    X_pca_test = score_train(idx_test, :);
+    Y_test = Y(idx_test);
+    Mdl = fitcnb(X_pca_train, Y_train);
+    Yhat_pca = predict(Mdl, X_pca_test);
+    precision_pca_nb(i) = mean(Yhat_pca == Y_test);
+end
+precision_pca_nb_ave = mean(precision_pca_nb);
