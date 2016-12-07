88261360dd5a9f555eb8e93f3c2888c6b89a55db
Owen Zhang <owenqyzhang@gmail.com>
2016-12-07 17:26:07 -0500

    Ignored some features that doesn't make sense

--- predict_labels.m
+++ predict_labels.m
@@ -9,14 +9,12 @@ function [Y_hat] = predict_labels(word_counts, cnn_feat, prob_feat, color_feat, 
 %           raw_tweets      nx1 cells containing all the raw tweets in text
 % Outputs:  Y_hat           nx1 predicted labels (1 for joy, 0 for sad)
 
-load ./models/log_ori_full.mat
+load ./models/log_ori.mat
 addpath('./liblinear/')
 
-n = size(word_counts, 1);
-[~, ~, prob_estimates] = predict(ones(n, 1), sparse(word_counts), log_ori_full, ['-q', 'col']);
+word_counts = word_counts(:, 5: end);
 
-Y_hat = zeros(4500, 1);
-[~, ind] = sort(prob_estimates, 'descend');
-Y_hat(ind(1: 2500)) = 1;
+n = size(word_counts, 1);
+Y_hat = predict(ones(n, 1), sparse(word_counts), log_ori, ['-q', 'col']);
 
 end
--- words_train_log.m
+++ words_train_log.m
@@ -2,40 +2,40 @@ clear
 clc
 
 load ./data/train_set/words_train.mat
-X1 = full(X);
-load ./data/train_set_unlabeled/words_train_unlabeled.mat
-X2 = full(X);
+% X1 = full(X);
+% X1 = X1(:, 5: end);
+% load ./data/train_set_unlabeled/words_train_unlabeled.mat
+% X2 = full(X);
 
 %% Logistic Regression
-addpath('liblinear/');
+% addpath('liblinear/');
 
-log_ori = train(full(Y), sparse(X1), ['-s 0', 'col']);
+% log_ori = train(full(Y), sparse(X1), ['-s 0', 'col']);
 
-[y_unlabeled_est, ~, prob_estimates] = predict(ones(4500, 1), sparse(X2), log_ori, ['-q', 'col']);
+% [y_unlabeled_est, ~, prob_estimates] = predict(ones(4500, 1), sparse(X2), log_ori, ['-q', 'col']);
 
-idx = 1: 4500;
-ind_unlabeled_inlier = idx(abs(prob_estimates) > 1);
-X2_inlier = X2(ind_unlabeled_inlier, :);
-Y_unlabeled_inlier = y_unlabeled_est(ind_unlabeled_inlier);
+% idx = 1: 4500;
+% ind_unlabeled_inlier = idx(abs(prob_estimates) > 1);
+% X2_inlier = X2(ind_unlabeled_inlier, :);
+% Y_unlabeled_inlier = y_unlabeled_est(ind_unlabeled_inlier);
 
-X = [X1; X2];
-Y = [full(Y); y_unlabeled_est];
+% X = [X1; X2];
+% Y = [full(Y); y_unlabeled_est];
 
-log_ori_full = train(full(Y), sparse(X), ['-s 0', 'col']);
-save('./models/log_ori_full.mat', 'log_ori_full', '-v7.3');
+% log_ori_full = train(full(Y), sparse(X), ['-s 0', 'col']);
+% save('./models/log_ori_full.mat', 'log_ori_full', '-v7.3');
 
-% precision_ori_log = zeros(9, 1);
 % ind = crossvalind('Kfold', 4500, 10);
+% acc = 0;
 % for i = 1: 10
-%     idx = 1: 9000;
+%     idx = 1: 4500;
 %     idx_test = find(ind == i);
-%     idx_train = idx;
-%     idx_train(idx_test) = [];
+%     idx_train = find(ind ~= i);
 %     
 %     model = train(full(Y(idx_train)), sparse(X(idx_train, :)),...
 %         ['-s 0', 'col']);
 %     Yhat = predict(ones(450, 1), sparse(X(idx_test, :)),...
 %         model, ['-q', 'col']);
-%     precision_ori_log(i) = mean(Yhat == Y(idx_test));
+%     acc = acc + mean(Yhat == Y(idx_test));
 % end
-% precision_ori_log_ave = mean(precision_ori_log);
+Yhat = predict_labels(X, [], [], [], [], []);
